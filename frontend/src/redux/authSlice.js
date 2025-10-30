c0960c8c64651893d5db632004aa3e2098cc6726
sarfarazkasmani <yusufkasmani491@gmail.com>
2025-10-30 10:23:22 +0530

    authslice updated

--- frontend/src/redux/authSlice.js
+++ frontend/src/redux/authSlice.js
@@ -5,6 +5,7 @@ const authSlice = createSlice({
   initialState: {
     user: null,
     suggestedUsers: [],
+    selectedUser: null,
     userProfile: null,
   },
   reducers: {
@@ -18,10 +19,17 @@ const authSlice = createSlice({
     setUserProfile: (state, action) => {
       state.userProfile = action.payload;
     },
+    setSelectedUser: (state, action) => {
+      state.selectedUser = action.payload;
+    },
   },
 });
 
-export const { setAuthUser, setSuggestedUsers, setUserProfile } =
-  authSlice.actions;
+export const {
+  setAuthUser,
+  setSuggestedUsers,
+  setUserProfile,
+  setSelectedUser,
+} = authSlice.actions;
 
 export default authSlice.reducer;
